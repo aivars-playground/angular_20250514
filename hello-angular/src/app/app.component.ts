646647ff1819c2187d48fd0fd350fb2b28f23fd6
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-05-14 20:13:47 +0100

    do not rename root AppComponent

---
+++ hello-angular/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {AboutComponent} from './about/about.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [AboutComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+}
