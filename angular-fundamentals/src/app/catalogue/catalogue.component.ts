92108d138526dbefbf53657ad9507f3cb1891464
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-05-18 18:55:35 +0100

    adding dynamic values

--- angular-fundamentals/src/app/catalogue/catalogue.component.ts
+++ angular-fundamentals/src/app/catalogue/catalogue.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {IProduct} from './product/product.model';
 
 @Component({
   selector: 'app-catalogue',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalogue.component.css']
 })
 export class CatalogueComponent {
+  product: IProduct
 
+  constructor() {
+    this.product =   {
+      id: 1,
+      description: "A robot head with an unusually large eye and teloscpic neck -- excellent for exploring high spaces.",
+      name: "Large Cyclops",
+      imageName: "head-big-eye.png",
+      category: "Heads",
+      price: 1220.5,
+      discount: 0.2,
+    }
+  }
 }
