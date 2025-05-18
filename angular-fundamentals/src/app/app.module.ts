ff2d116b9e3191c04298494339000cb81310c7dd
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-05-18 18:02:18 +0100

    adding dynamic values

--- angular-fundamentals/src/app/app.module.ts
+++ angular-fundamentals/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import {SiteHeaderComponent} from './site-header/site-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    CatalogueComponent,
+    SiteHeaderComponent
   ],
   imports: [
     BrowserModule
