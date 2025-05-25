a9912427fac384cd1f0bd6328adf5713ff161f46
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-05-25 15:38:08 +0100

    adding router

--- angular-fundamentals/src/app/app-routing.module.ts
+++ angular-fundamentals/src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {CatalogueComponent} from "./catalog/catalogue.component";
+import {CartComponent} from "./cart/cart.component";
 
-
+const routes: Routes = [
+  {path: "home", component: HomeComponent, title: "SHOP|Home"},
+  {path: "catalog", component: CatalogueComponent, title: "SHOP|Catalog"},
+  {path: "cart", component: CartComponent, title: "SHOP|Cart"},
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
